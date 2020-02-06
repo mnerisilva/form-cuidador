ffa6e5578985f64b41413475d4fb31be7e48f6f0
Marcelo Neri da Silva <marceloneri@gmail.com>
2020-02-06 00:46:36 -0300

    testes

--- src/components/formcuidador/FormCuidador.js
+++ src/components/formcuidador/FormCuidador.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 import Form from './Form'
 
-let a = 100
-
 const FormCuidador = () => {
     return (  
         <div> 
             <Form />
-            <p>O valor de "a" é igual a {a}</p>
         </div>     
     );
 }
