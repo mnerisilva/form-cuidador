77354db5818b218660df6079bf52b7f3b5a34c3a
Marcelo Neri da Silva <marcelo.silva@sc.senac.br>
2020-02-06 11:14:27 -0300

    teste

--- src/components/formcuidador/Form.js
+++ src/components/formcuidador/Form.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
+import TabHome from './tabs/TabHome'
+import TabProfile from './tabs/TabProfile'
+import TabContact from './tabs/TabContact'
+
+
 const Form = () => {
     return (
         <div className="container">
-            <form className="form-horizontal">
-                <fieldset>
-                <legend>Candidato</legend>
-                <div class="form-group">
-                    <label htmlFor="formGroupExampleInput">Example label</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder" />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="formGroupExampleInput2">Another label</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" />
-                </div>
-                <div class="form-group">
-                    <label className="col-md-6 control-label" for="textinput">Nome</label>  
-                    <div className="col-md-6">
-                        <input id="textinput" name="textinput" type="email" placeholder="nome" className="form-control input-md" required="" />
-                        <span className="help-block">Insira seu nome</span>  
-                </div>
-</div>
-                </fieldset>
-            </form>
+            <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item">
+                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+            </li>
+            </ul>
+            <div className="tab-content" id="myTabContent">
+                <TabHome />
+                <TabProfile />
+                <TabContact />
+            </div>            
         </div>      
     )
 }
