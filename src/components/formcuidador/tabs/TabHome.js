c5597886169ab8506f3183cb15de5fb7a28b5dc4
Marcelo Neri da Silva <marceloneri@gmail.com>
2020-02-06 23:48:03 -0300

    mais campos form

--- src/components/formcuidador/tabs/TabHome.js
+++ src/components/formcuidador/tabs/TabHome.js
@@ -19,6 +19,52 @@ const TabHome = () => {
                         <input type="email" className="form-control" id="nome" placeholder="" />
                     </div>
                 </div>
+                <div className="row">
+                    <div className="form-group col-md-4">
+                        <label htmlFor="rg">RG</label>
+                        <input type="text" className="form-control" id="rg" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="emissao">Emissão</label>
+                        <input type="text" className="form-control" id="emissao" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="orgao">Orgão Emissor</label>
+                        <input type="text" className="form-control" id="orgao" placeholder="" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="form-group col-md-4">
+                        <label htmlFor="endereco">Endereço</label>
+                        <input type="text" className="form-control" id="endereco" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="numero">Número</label>
+                        <input type="text" className="form-control" id="numero" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="complemento">Complemento</label>
+                        <input type="text" className="form-control" id="complemento" placeholder="" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="form-group col-md-4">
+                        <label htmlFor="bairro">Bairro</label>
+                        <input type="text" className="form-control" id="bairro" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label htmlFor="cep">Cep</label>
+                        <input type="text" className="form-control" id="cep" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="municipio">Município</label>
+                        <input type="text" className="form-control" id="municipio" placeholder="" />
+                    </div>
+                    <div className="form-group col-md-1">
+                        <label htmlFor="uf">UF</label>
+                        <input type="text" className="form-control" id="uf" placeholder="" />
+                    </div>
+                </div>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlSelect1">Example select</label>
                     <select className="form-control" id="exampleFormControlSelect1">
