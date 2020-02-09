e87f2b7cda584913dc68f74acab0c4def416aa40
Marcelo Neri da Silva <marceloneri@gmail.com>
2020-02-09 06:34:19 -0300

    mais ajustes

--- src/components/formcuidador/Form.js
+++ src/components/formcuidador/Form.js
@@ -7,7 +7,7 @@ import TabContact from './tabs/TabContact'
 
 const Form = () => {
     return (
-        <div className="container">
+        <div className="container" style={{marginTop: 15}}>
             <ul className="nav nav-tabs" id="myTab" role="tablist">
                 <li className="nav-item">
                     <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Pessoal</a>
